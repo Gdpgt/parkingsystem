cbf83a8035785a34577f227cad750096a8ab329b
Guillaume de Puget <guillaumedepuget@gmail.com>
2025-02-01 17:06:53 +0100

    test: terminé 3 tests unitaires de processIncomingVehicle

--- src/main/java/com/parkit/parkingsystem/service/ParkingService.java
+++ src/main/java/com/parkit/parkingsystem/service/ParkingService.java
@@ -52,8 +52,8 @@ public class ParkingService {
                 }
 
                 System.out.println("Generated Ticket and saved in DB\n");
-                System.out.println("Please park your vehicle in spot number:"+parkingSpot.getId());
-                System.out.println("\nRecorded in-time for vehicle number:" + vehicleRegNumber+" is:" + inTime + '\n');
+                System.out.println("Please park your vehicle in spot number: "+parkingSpot.getId());
+                System.out.println("\nRecorded in-time for vehicle number: " + vehicleRegNumber+" is: " + inTime + '\n');
             }
 
         }catch(Exception e){
@@ -66,7 +66,7 @@ public class ParkingService {
         return inputReaderUtil.readVehicleRegistrationNumber();
     }
 
-    public ParkingSpot getNextParkingNumberIfAvailable(){
+    private ParkingSpot getNextParkingNumberIfAvailable(){
         int parkingNumber=0;
         ParkingSpot parkingSpot = null;
         try{
@@ -90,18 +90,14 @@ public class ParkingService {
         System.out.println("1 CAR");
         System.out.println("2 BIKE\n");
         int input = inputReaderUtil.readSelection();
-        switch(input){
-            case 1: {
-                return ParkingType.CAR;
-            }
-            case 2: {
-                return ParkingType.BIKE;
-            }
-            default: {
+        return switch (input) {
+            case 1 -> ParkingType.CAR;
+            case 2 -> ParkingType.BIKE;
+            default -> {
                 System.out.println("Incorrect input provided");
                 throw new IllegalArgumentException("Entered input is invalid");
             }
-        }
+        };
     }
 
     public void processExitingVehicle() {
@@ -130,7 +126,7 @@ public class ParkingService {
         }catch(Exception e){
             logger.error("Unable to process exiting vehicle",e);
         }
-}
+    }
 
     private void displayExitMessage(Ticket ticket, boolean isDiscounted) {
         if (ticket.getPrice() == 0.0) {
