a370ee2af0b2f5a5a1d7dbd2051dfeb27911c629
DE PUGET Guillaume <guillaumedepuget@gmail.com>
2025-02-06 19:40:22 +0100

    WIP: tests d'intgération

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -1,9 +1,11 @@
 package com.parkit.parkingsystem.integration;
 
+import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
+import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.FareCalculatorService;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
@@ -14,12 +16,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
 
-    private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
+    private static DataBaseTestConfig dataBaseTestConfig;
     private static ParkingSpotDAO parkingSpotDAO;
     private static TicketDAO ticketDAO;
     private static FareCalculatorService fareCalculatorService;
@@ -30,6 +38,7 @@ public class ParkingDataBaseIT {
 
     @BeforeAll
     public static void setUp() throws Exception{
+        dataBaseTestConfig = new DataBaseTestConfig();
         parkingSpotDAO = new ParkingSpotDAO();
         parkingSpotDAO.dataBaseConfig = dataBaseTestConfig;
         ticketDAO = new TicketDAO();
@@ -51,22 +60,38 @@ public class ParkingDataBaseIT {
         ticketDAO = null;
         dataBasePrepareService = null;
         fareCalculatorService = null;
-        inputReaderUtil = null;
     }
 
     @Test
-    public void testParkingACar(){
+    void testParkingACar(){
+        // Arrange
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO, fareCalculatorService);
+
+        // Act
         parkingService.processIncomingVehicle();
-        //TODO: check that a ticket is actually saved in DB and Parking table is updated with availability
+
+        // Assert
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        assertNotNull(ticket, "Le ticket aurait dû être généré.");
+        assertNotNull(ticket.getInTime(), "L'heure d'entrée n'a pas été enregistrée.");
+        assertNull(ticket.getOutTime(), "L'heure de sortie ne devrait pas être renseignée.");
+        assertFalse(ticket.getParkingSpot().isAvailable()
+        , "La place de parking aurait dû être marquée comme occupée.");
+        assertEquals(ticket.getParkingSpot().getId(), parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR),
+                "La place de parking attribuée ne correspond pas à celle attendue.");
+        assertTrue(ticketDAO.getNbTickets("ABCDEF") > 0
+        , "Le ticket n'a pas été sauvegardé en base.");
     }
 
     @Test
-    public void testParkingLotExit(){
+    void testParkingLotExit(){
         testParkingACar();
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO, fareCalculatorService);
         parkingService.processExitingVehicle();
-        //TODO: check that the fare generated and out time are populated correctly in the database
+        Ticket ticket = ticketDAO.getTicket("ABCDEF");
+        assertNotNull(ticket.getOutTime());
+        assertTrue(ticket.getParkingSpot().isAvailable());
     }
+
 
 }
